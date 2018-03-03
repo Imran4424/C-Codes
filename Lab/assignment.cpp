f9fad9cd4063cae8bbdf160fbfd983ca0c38ba00
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-03-03 15:53:56 +0600

    lab assignment

---
+++ Lab/assignment.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class firstprogramincplusplus{
+public:
+    void print()
+    {
+        cout<<"Wellcome to C++ world"<<endl;
+    }
+};
+
+int main()
+{
+    firstprogramincplusplus obj;
+    obj.print();
+
+    return 0;
+}
