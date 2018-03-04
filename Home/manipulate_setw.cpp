a11d4e6f31ce623b7a36a1c0983a4478ef5da97a
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-03-05 00:10:29 +0600

    setw implementation

---
+++ Home/manipulate_setw.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    cout<<setw(6)<<10<<endl;
+    cout<<setw(6)<<100<<endl;
+    cout<<setw(6)<<1000<<endl;
+    cout<<setw(6)<<10000<<endl;
+    cout<<setw(6)<<100000<<endl;
+}
